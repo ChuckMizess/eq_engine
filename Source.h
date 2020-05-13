cbe0ec9577d80c54a7a34431db020cc6c287c64f
Vital V <you@example.com>
2020-05-13 12:46:56 +0300

    Объявление методов перенесено в файл-хэдер (Souce.h) Создана справка DOC  в конце файла Стоит подумать над размещением определения функций до main() или после main()

--- Source.h
+++ Source.h
@@ -4,3 +4,10 @@
 #include<string>
 #include<math.h>
 
+void inputData(int& var_x1,const char* str_x1, int& var_y1, const char* str_y1, int& var_b1,
+    const char* str_b1);
+ 
+double getDet(int& x1, int& x2, int& y1, int& y2);
+
+void outputData(float& detA, const char* str_detA, float& detX, const char* str_detX, float& detY, const char* str_detY);
+
